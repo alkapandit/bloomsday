7cbac45e775f09cabe7857c00f85108b43480f53
alka-aess <alka.pandit@aesssolutions.com>
2024-03-23 18:27:38 +0530

    main section degin completed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+		primaryGreen:"#1C4835",
+		secondaryGreen:"#208459",
+		lightGreen:"#9FBCAD",
+        blackish: "#191919",
+        blackishV2: "#1f1f1f",
+        yellowish: "#F5F2E1",
+        greyish: "#979592",
+        greyishV2: "#28292A",
+      },
+	  fontFamily: {
+		breeSerif: ["'Bree Serif'", "serif"],
+		neueMontreal: ["'Neue Montreal'", "sans-serif"],
+	  },
+    },
+  },
+  plugins: [],
+};
